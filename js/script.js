624aada69dbd24b69395737eb80641c38381cb4c
Sanjeev Kumar <skj5793@gmail.com>
2026-08-24 14:54:01 +0530

    Refactoring, default theme and sample videos uploads

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+  document.getElementById("year").textContent = new Date().getFullYear();
+
   /* ---------------------------------------------------------
      Theme Toggle
      --------------------------------------------------------- */
